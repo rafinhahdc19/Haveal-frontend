ecb1f4cc098bdebb30acef3d4a5996da71f3a787
rafinhahdc19 <124531634+rafinhahdc19@users.noreply.github.com>
2023-08-29 16:49:02 -0300

    modification build 3

--- src/pages/api/auth/[...nextauth].jsx
+++ src/pages/api/auth/[...nextauth].jsx
@@ -1,39 +1,38 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
 import axios from "axios";
-import Cookies from 'js-cookie';
+import { setCookie } from "nookies";
 
 export default NextAuth({
     providers: [
         GoogleProvider({
-            
             clientId: process.env.CLIENTIDGOOGLE,
             clientSecret: process.env.CLIENTSECRETIDGOOGLE,
-            
         })
     ],
     secret: process.env.SECRET,
     callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-                const response = await axios.post('https://haveal-backend.vercel.app/login/provider', {
-                    "nome": user.name,
-                    "email": user.email
+        async signIn({ user, account, profile, email, credentials, res }) {
+            const response = await axios.post('https://haveal-backend.vercel.app/login/provider', {
+                "nome": user.name,
+                "email": user.email
+            });
+            if (response.data.user[0].jwt && response.data.user[0].nome && response.data.user[0].email) {
+                user = {
+                    id: response.data.user[0].id,
+                    name: response.data.user[0].nome,
+                    email: response.data.user[0].email,
+                    jwt: response.data.user[0].jwt
+                };
+                setCookie({ res }, "jwt", JSON.stringify(user), {
+                    maxAge: 2 * 24 * 60 * 60,
+                    path: "/",
+                    httpOnly: true,
                 });
-                if (response.data.user[0].jwt && response.data.user[0].nome && response.data.user[0].email) {
-                    user = {
-                        id: response.data.user[0].id,
-                        name: response.data.user[0].nome,
-                        email: response.data.user[0].email,
-                        jwt: response.data.user[0].jwt
-                    };
-                    Cookies.set('jwt', response.data.jwt, { expires: 30 });
-                    return user;
-                } else {
-                    return null;
-                }
-                
-            
+                return user;
+            } else {
+                return null;
+            }
         },
     },
 });
